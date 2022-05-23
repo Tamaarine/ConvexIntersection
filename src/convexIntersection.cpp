67593cfc266dce5a6ba302814434203f44005bd1
Tamarine <irebootplaygt@gmail.com>
2022-05-23 18:32:23 +0000

    Test case for the polygon intersection

--- src/convexIntersection.cpp
+++ src/convexIntersection.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include "polygon.h"
 #include "utils.h"
+#include <vector>
 
 using namespace std;
 
@@ -11,20 +12,28 @@ int main() {
     
     Point p2(1,2);
 
-    std::cout << p1 << "\n";
-    std::cout << p2 << "\n";
-    std::cout << subtract(p1, p2) << "\n";
+    cout << p1 << "\n";
+    cout << p2 << "\n";
+    cout << subtract(p1, p2) << "\n";
     
     Polygon poly1;
     Polygon poly2;
     
-    vector<Point> list1 = {Point(0,2), Point(0,0), Point(1.5,0), Point(1.5,2)};
-    vector<Point> list2 = {Point(2,1), Point(2,0), Point(1,0), Point(1,1)};
+    vector<Point> list1 = {Point(2,3), Point(-0.267,1.777), Point(0.68,-0.60), Point(3.35,-0.49), Point(5.53,1.1), Point(3.7,2.7)};
+    vector<Point> list2 = {Point(2.86,3.3), Point(2.38,1.71), Point(4.1,0.87), Point(5.2,1.77), Point(5.92,3.12), Point(3.8,3.44)};
     
     poly1.vertices = list1;
     poly2.vertices = list2;
     
-    convexIntersect(poly1, poly2);
+    vector<Point> intersections;
+    
+    convexIntersect(poly1, poly2, intersections);
+    
+    Polygon inters;
+    inters.vertices = intersections;
+    
+    cout << "the final intersection\n";
+    cout << inters;
     
     return 0;
 }
